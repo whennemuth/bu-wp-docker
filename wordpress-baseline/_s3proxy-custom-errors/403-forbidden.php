d84c314a5f8d961ac5a19ba148679298bd95473c
Jonathan Williams <tamlin@mac.com>
2024-03-13 14:17:44 -0400

    add custom apache error handling
    for the S3 proxy responses.

---
+++ wordpress-baseline/_s3proxy-custom-errors/403-forbidden.php
@@ -0,0 +1,7 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>403 Forbidden</title>
+</head><body>
+<h1>403 Forbidden</h1>
+<p>You don't have permission to access the resource at <?php echo $_SERVER['REDIRECT_SCRIPT_URL'] ?></p>  
+</body></html>
